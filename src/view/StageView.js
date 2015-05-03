2ea9821bcd15cd45e3da802c2926a703547554fd
Ates Goral <ates.g@myplanet.com>
2015-05-03 01:34:06 -0400

    Variable renaming

--- src/view/StageView.js
+++ src/view/StageView.js
@@ -17,47 +17,46 @@ var M_TO_PX = CANVAS_WIDTH / 100;
 function StageView(stage) {
     this.stage = stage;
 
-    var debugDrawCanvas = document.createElement('canvas');
-    debugDrawCanvas.width = CANVAS_WIDTH;
-    debugDrawCanvas.height = CANVAS_HEIGHT;
-    debugDrawCanvas.style.position = 'absolute';
-    debugDrawCanvas.style.left = 0;
-    debugDrawCanvas.style.top = 0;
-    debugDrawCanvas.style.background = '#e0e0e0';
-    document.body.appendChild(debugDrawCanvas);
+    var canvas = document.createElement('canvas');
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+    canvas.style.position = 'absolute';
+    canvas.style.left = 0;
+    canvas.style.top = 0;
+    document.body.appendChild(canvas);
 
-    this.canvas = debugDrawCanvas;
+    this.canvas = canvas;
 
-    var debugDrawCanvasContext = debugDrawCanvas.getContext("2d");
-    debugDrawCanvasContext.translate(0, debugDrawCanvas.height / 2);
-    debugDrawCanvasContext.scale(1, -1);
+    var ctx = canvas.getContext("2d");
+    ctx.translate(0, canvas.height / 2);
+    ctx.scale(1, -1);
 
     var debugDraw = new b2DebugDraw();
-    debugDraw.SetSprite(debugDrawCanvasContext);
+    debugDraw.SetSprite(ctx);
     debugDraw.SetDrawScale(M_TO_PX);
     debugDraw.SetFillAlpha(0.3);
     debugDraw.SetLineThickness(1.0);
     debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit | b2DebugDraw.e_centerOfMassBit);
     this.stage.world.SetDebugDraw(debugDraw);
 
-    this.debugDrawCanvasContext = debugDrawCanvasContext;
+    this.ctx = ctx;
 
     var isMouseDown = false;
 
-    debugDrawCanvas.onmousedown = function (e) {
+    canvas.onmousedown = function (e) {
         e.preventDefault();
 
         isMouseDown = true;
-        this.stage.setTarget(e.pageX / M_TO_PX, (debugDrawCanvas.height / 2 - e.pageY) / M_TO_PX);
+        this.stage.setTarget(e.pageX / M_TO_PX, (canvas.height / 2 - e.pageY) / M_TO_PX);
 
         return false;
     }.bind(this);
 
-    debugDrawCanvas.onmousemove = function (e) {
+    canvas.onmousemove = function (e) {
         e.preventDefault();
 
         if (isMouseDown) {
-            this.stage.setTarget(e.pageX / M_TO_PX, (debugDrawCanvas.height / 2 - e.pageY) / M_TO_PX);
+            this.stage.setTarget(e.pageX / M_TO_PX, (canvas.height / 2 - e.pageY) / M_TO_PX);
         }
 
         return false;
@@ -72,7 +71,7 @@ function StageView(stage) {
 }
 
 StageView.prototype.render = function () {
-    this.debugDrawCanvasContext.clearRect(0, -CANVAS_HEIGHT / 2, CANVAS_WIDTH, CANVAS_HEIGHT / 2);
+    this.ctx.clearRect(0, -CANVAS_HEIGHT / 2, CANVAS_WIDTH, CANVAS_HEIGHT / 2);
     this.stage.world.DrawDebugData();
 };
 
